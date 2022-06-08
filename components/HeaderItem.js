4aac8bf50b032f793be9edce67f3077dee5c1474
BatoolBtoush <batoolbtoush98@gmail.com>
2022-06-08 22:08:12 +0300

    half way through building the header

---
+++ components/HeaderItem.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function HeaderItem({title, Icon}) {
+  return (
+    <div className='flex flex-col items-center w-12 cursor-pointer group sm:w-20 hover:text-white'>
+      <Icon className='h-8 mb-1'/>
+      <p className='tracking-widest opacity-0 group-hover:opacity-100 group-hover:animate-bounce' >{title}</p>
+    </div>
+  )
+}
+
+export default HeaderItem
