a314674b08c52877fe12c73f8b565aa28d92489f
BatoolBtoush <batoolbtoush98@gmail.com>
2022-06-09 03:04:30 +0300

    done with Header

--- components/Header.js
+++ components/Header.js
@@ -14,9 +14,15 @@ import HeaderItem from './HeaderItem'
 
 function Header() {
   return (
-    <header className=''>
-       <div >
+    <header className='flex flex-col items-center justify-between h-auto m-5 sm:flex-row'>
+       <div className='flex flex-grow max-w-2xl justify-evenly'>
            <HeaderItem title='HOME' Icon={HomeIcon} />
+           <HeaderItem title='TRENDING' Icon={LightningBoltIcon} />
+           <HeaderItem title='VERIFIED' Icon={BadgeCheckIcon} />
+           <HeaderItem title='COLLECTIONS' Icon={CollectionIcon} />
+            <HeaderItem title='SEARCH' Icon={SearchIcon} />
+            <HeaderItem title='ACCOUNT' Icon={UserIcon} />
+            
 
        </div>
         <Image 
--- components/HeaderItem.js
+++ components/HeaderItem.js
@@ -3,8 +3,8 @@ import React from 'react'
 function HeaderItem({title, Icon}) {
   return (
     <div className='flex flex-col items-center w-12 cursor-pointer group sm:w-20 hover:text-white'>
-      <Icon className='h-8 mb-1'/>
-      <p className='tracking-widest opacity-0 group-hover:opacity-100 group-hover:animate-bounce' >{title}</p>
+      <Icon className='h-8 mb-1 group-hover:animate-bounce'/>
+      <p className='tracking-widest opacity-0 group-hover:opacity-100' >{title}</p>
     </div>
   )
 }
